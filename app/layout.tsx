2098a9747146b8514283faa66277269f8c1a1559
李国庆 <478744551@qq.com>
2023-05-22 12:05:42 +0800

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "为什么不问问神奇海螺呢",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
